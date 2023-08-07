8beb5fc413a03cbd65a0511446f02395da8026dc
jayylmao <jaymichael_carlos@icloud.com>
2023-08-07 15:03:54 +0800

    feat: add assets + new ui

---
+++ src/main/java/com/example/lbycpeifinalprojectgachamachine/Item.java
@@ -0,0 +1,7 @@
+package com.example.lbycpeifinalprojectgachamachine;
+
+public abstract class Item implements ItemInterface {
+    public double rarity;
+    public String name;
+    public String description;
+}
---
+++ src/main/java/com/example/lbycpeifinalprojectgachamachine/ItemInterface.java
@@ -0,0 +1,4 @@
+package com.example.lbycpeifinalprojectgachamachine;
+
+public interface ItemInterface {
+}
